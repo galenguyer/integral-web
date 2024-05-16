14f857558e9084cd523cc33c60847752ae3cd1f3
Galen Guyer <galen@galenguyer.com>
2024-05-15 21:12:07 -0400

    add comments when job is created

--- src/pages/NewJob.tsx
+++ src/pages/NewJob.tsx
@@ -1,10 +1,15 @@
 import { Button, Center, Stack, TextInput } from "@mantine/core";
 import { RequireAuth, useAuth } from "../hooks/useAuth";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const NewJobPage = () => {
     const [synopsis, setSynopsis] = useState('');
     const [location, setLocation] = useState('');
+    const [callerName, setCallerName] = useState('');
+    const [callerPhone, setCallerPhone] = useState('');
+    const [comments, setComments] = useState('');
+    const navigate = useNavigate();
 
     const auth = useAuth();
 
@@ -17,9 +22,10 @@ const NewJobPage = () => {
             },
             body: JSON.stringify({
                 synopsis: synopsis,
-                location: location
+                location: location,
+                comments: [`Caller: ${callerName} - ${callerPhone}`, comments].map((s: string) => s.trim()).filter((s) => s != '')
             })
-        })
+        }).then((res) => res.json()).then((data) => navigate(`/job/${data.id}`))
     }
 
     return (<RequireAuth>
@@ -37,6 +43,22 @@ const NewJobPage = () => {
                     description="Location of the event"
                     value={location}
                     onChange={(event) => setLocation(event.currentTarget.value)}
+                />
+                <TextInput miw='400'
+                    label="Caller Name"
+                    value={callerName}
+                    onChange={(event) => setCallerName(event.currentTarget.value)}
+                />
+                <TextInput miw='400'
+                    label="Caller Phone Number"
+                    value={callerPhone}
+                    onChange={(event) => setCallerPhone(event.currentTarget.value)}
+                />
+                <TextInput miw='400'
+                    label="Comments"
+                    description="Any additional information"
+                    value={comments}
+                    onChange={(event) => setComments(event.currentTarget.value)}
                 />
                 <Button onClick={() => handleSubmit()} fullWidth>Create Job</Button>
             </Stack>
