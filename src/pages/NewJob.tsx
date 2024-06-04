d5308827b540168ed400e00bd9c40883eee7cf46
Galen Guyer <galen@galenguyer.com>
2024-06-04 11:58:51 -0400

    Use form for new job, simple validation

--- src/pages/NewJob.tsx
+++ src/pages/NewJob.tsx
@@ -1,77 +1,86 @@
-import { Button, Group, Stack, TextInput } from '@mantine/core';
+import { Button, Group, TextInput } from '@mantine/core';
 import { RequireAuth, useAuth } from '../hooks/useAuth';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { isNotEmpty, useForm } from '@mantine/form';
 
 const NewJobPage = () => {
-  const [synopsis, setSynopsis] = useState('');
-  const [location, setLocation] = useState('');
-  const [callerName, setCallerName] = useState('');
-  const [callerPhone, setCallerPhone] = useState('');
-  const [comments, setComments] = useState('');
   const navigate = useNavigate();
 
   const auth = useAuth();
 
-  const handleSubmit = () => {
+  const handleSubmit = (values: any) => {
+    console.log(values);
+    const payload: any = {
+      ...values,
+      comments: [values.comments].map((s: string) => s.trim())
+        .filter((s) => s != ''),
+    }
     fetch('/api/v0/jobs', {
       method: 'POST',
       headers: {
         Authorization: `Bearer ${auth.token}`,
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        synopsis: synopsis,
-        location: location,
-        callerName: callerName,
-        callerPhone: callerPhone,
-        comments: [comments]
-          .map((s: string) => s.trim())
-          .filter((s) => s != ''),
-      }),
+      body: JSON.stringify(payload),
     })
       .then((res) => res.json())
       .then((data) => navigate(`/jobs/${data.id}`));
   };
 
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      synopsis: '',
+      location: '',
+      callerName: '',
+      callerPhone: '',
+      comments: '',
+    },
+    validate: {
+      synopsis: isNotEmpty('Synopsis cannot be empty'),
+      location: isNotEmpty('Location cannot be empty')
+    }
+  });
+
   return (
     <RequireAuth>
-      <Stack>
-        <h2>New Job</h2>
+      <h2>Create Job</h2>
+      <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
         <TextInput
+          withAsterisk
           label="Synopsis"
-          description="A brief description of the call"
-          value={synopsis}
-          onChange={(event) => setSynopsis(event.currentTarget.value)}
+          description="A brief description of the job"
+          key={form.key('synopsis')}
+          {...form.getInputProps('synopsis')}
         />
-        <TextInput
+        <TextInput mt='sm'
+          withAsterisk
           label="Location"
           description="Location of the event"
-          value={location}
-          onChange={(event) => setLocation(event.currentTarget.value)}
+          key={form.key('location')}
+          {...form.getInputProps('location')}
         />
-        <Group grow gap="xl">
+        <Group grow mt='sm' gap="xl">
           <TextInput
             label="Caller Name"
-            value={callerName}
-            onChange={(event) => setCallerName(event.currentTarget.value)}
+            key={form.key('callerName')}
+            {...form.getInputProps('callerName')}
           />
           <TextInput
             label="Caller Phone Number"
-            value={callerPhone}
-            onChange={(event) => setCallerPhone(event.currentTarget.value)}
+            key={form.key('callerPhone')}
+            {...form.getInputProps('callerPhone')}
           />
         </Group>
         <TextInput
+          mt='sm'
           label="Comments"
           description="Any additional information"
-          value={comments}
-          onChange={(event) => setComments(event.currentTarget.value)}
+          key={form.key('comments')}
+          {...form.getInputProps('comments')}
         />
-        <Button onClick={() => handleSubmit()} fullWidth>
-          Create Job
-        </Button>
-      </Stack>
+        <Button mt='lg' fullWidth type="submit">Submit</Button>
+      </form>
     </RequireAuth>
   );
 };
